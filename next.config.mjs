365bff0545c29e865f4c7278c5cc230de26ce93d
Veikko <kiis.veikko@gmail.com>
2026-07-13 12:36:18 +0300

    screenshots fix test

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
+  basePath: "/vk-portfolio-github",
+  assetPrefix: "/vk-portfolio-github",
   images: {
     unoptimized: true,
   },
