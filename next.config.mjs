5789a606f5fc625d0cd45bf8aa7d2ae4bd85d25d
Veikko <kiis.veikko@gmail.com>
2026-07-15 01:23:28 +0300

    Update GitHub link for portfolio project

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/vk-portfolio-github",
-  assetPrefix: "/vk-portfolio-github",
-  images: { unoptimized: true },
-  env: {
-    NEXT_PUBLIC_BASE_PATH: "/vk-portfolio-github",
-  },
-};
+   const nextConfig = {
+     output: "export",
+     images: { unoptimized: true },
+   };
 
-export default nextConfig;
+   export default nextConfig;
